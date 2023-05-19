e42c6311acc9c09f2565e16f5a6574b84b9c9735
jonatangreen <111164591+jonatangreen@users.noreply.github.com>
2023-05-18 19:44:27 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   var documents = [
-    'https://docs.google.com/spreadsheets/d/1_d75C1agT45j6r6CS6FW5W-m37iokVOi/edit?usp=sharing&ouid=117383372408130911949&rtpof=true&sd=true',
-    'https://docs.google.com/document/d/1EFXN4pdbquNIHxZwTm9zVmTaCB55qDWD/edit?usp=sharing&ouid=117383372408130911949&rtpof=true&sd=true',
-    'https://docs.google.com/document/d/1affzNA5jLRsbv6UyGHEwcSE_7Dy22q6eALhoqOHzLfg/edit?usp=sharing'
+    'https://docs.google.com/spreadsheets/d/1_d75C1agT45j6r6CS6FW5W-m37iokVOi/preview',
+    'https://docs.google.com/document/d/1EFXN4pdbquNIHxZwTm9zVmTaCB55qDWD/preview',
+    'https://docs.google.com/document/d/1affzNA5jLRsbv6UyGHEwcSE_7Dy22q6eALhoqOHzLfg/preview'
   ];
 
   var currentDocument = 0;
 
   function loadDocument() {
     var documentUrl = documents[currentDocument];
-     $('#document-container').html('<iframe src="' + documentUrl + '&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>');
+    $('#document-container').html('<iframe src="' + documentUrl + '&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>');
 
     currentDocument++;
     if (currentDocument >= documents.length) {
