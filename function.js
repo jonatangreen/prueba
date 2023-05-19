1021352d00e547eb26095468d6a32febd61fbebe
jonatangreen <111164591+jonatangreen@users.noreply.github.com>
2023-05-18 20:42:48 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-  // Verificar si la página ya ha sido visitada
-if (document.cookie.includes('visited')) {
-  // La página ya ha sido visitada, no contarla nuevamente
-  console.log('Página ya visitada');
-} else {
-  // La página no ha sido visitada, contarla como una nueva vista
-  console.log('Nueva vista de página');
-
-  // Establecer una cookie para indicar que la página ha sido visitada
-  document.cookie = 'visited=true; path=/';
-}
 
   var documents = [
     'https://docs.google.com/document/d/1EFXN4pdbquNIHxZwTm9zVmTaCB55qDWD/edit?usp=sharing&ouid=117383372408130911949&rtpof=true&sd=true',
@@ -20,6 +9,17 @@ if (document.cookie.includes('visited')) {
   var currentDocument = 0;
 
   function loadDocument() {
+      // Verificar si la página ya ha sido visitada
+if (document.cookie.includes('visited')) {
+  // La página ya ha sido visitada, no contarla nuevamente
+  console.log('Página ya visitada');
+} else {
+  // La página no ha sido visitada, contarla como una nueva vista
+  console.log('Nueva vista de página');
+
+  // Establecer una cookie para indicar que la página ha sido visitada
+  document.cookie = 'visited=true; path=/';
+}
     var documentUrl = documents[currentDocument];
     $('#document-container').html('<iframe src="' + documentUrl + '&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>');
 
